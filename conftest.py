515891cddc1107b4178ae699ac74297da84f5c52
Daniil Tkalich <103593586+tdanielv@users.noreply.github.com>
2022-09-22 17:32:20 +0400

    finaly version

--- conftest.py
+++ conftest.py
@@ -1,1 +1,26 @@
-sdfsdf
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru', help='Choose language ')
+    parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser')
+
+@pytest.fixture(scope='function')
+def language(pytestconfig):
+    return pytestconfig.getoption('language')
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    # language = request.config.getoption('language')
+    browser = None
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome()
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError('browser should be chrome or firefox')
+    yield browser
+    print('\n quit browser')
+    browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,13 @@
-fasdf
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/'
+
+
+
+def test_store(browser, language):
+    browser.get(link+language+'/catalogue/coders-at-work_207/')
+    browser.find_element(By.CSS_SELECTOR, 'button.btn[value]').click()
+    assert browser.find_element(By.CSS_SELECTOR, 'div.alertinner'), 'Error'
+
